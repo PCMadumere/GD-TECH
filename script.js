22d712fea5aa094f517d87ea22813ae34ffdb9e3
LastBorn <madumerepreciouschukwuka@gmail.com>
2025-07-01 21:54:38 +0100

    commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const menu = 
-const menuOpen = 
-const menuClose = 
+const menu = document.querySelector('.nav-bar ul')
+const menuOpen = document.querySelector('.ri-menu-line')
+const menuClose = document.querySelector('.ri-close-circle-fill')
+
+menuOpen.addEventListener('click', function(){
+    menu.style.top = '13.6%';
+    menuOpen.style.display = 'none';
+    menuClose.style.display = 'block';
+})
+
+menuClose.addEventListener('click', function(){
+    menu.style.top = '-50%';
+    menuOpen.style.display = 'block';
+    menuClose.style.display = 'none';
+})
